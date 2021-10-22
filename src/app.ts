8d7a26628a36e20d4b45b8e41511a6de87a7ce63
Eduardo <eduardo.carlos.siqueira@gmail.com>
2021-10-22 11:36:23 -0300

    Mais Cors

--- src/app.ts
+++ src/app.ts
@@ -12,19 +12,11 @@ dotenv.config();
 
 const app: Express = express();
 
-app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 app.use("/api/usuarios", usuarios);
 app.use("/api/sorteio", sorteio);
